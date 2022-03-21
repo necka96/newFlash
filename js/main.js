adcc713b3bda0cabc9b4992da03b57185370caee
necka96 <nemanja2014@live.com>
2022-03-21 18:32:07 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,18 +3,13 @@ const selectElement = selector => {
  if(element) return element;
  throw new Error(`Uverite se da li ovaj ${selector} postoji ili da li je pogresno napisan`)
 }
-const constiner = selectElement(".container")
+const container = selectElement("#container")
 const hamburgerMenu = selectElement(".hamburgerMenu")
 hamburgerMenu.addEventListener("click", ()=> {
- constiner.classList.toggle("active")
+ container.classList.toggle("active")
  
 })
-const links = document.querySelectorAll(".links a")
-links.forEach((link)=>{
- link.addEventListener("click", ()=>{
-  constiner.classList.remove("active")
- })
-})
+
 
 const scrollHeader = () => {
  const navbarElement = selectElement(".navbar")
@@ -40,3 +35,42 @@ themeToggleBtn.addEventListener("click", ()=>{
     localStorage.removeItem("currentTheme")
  }
 })
+const fromOpenBtn= selectElement("#search-icon")
+const fromCloseBtn= selectElement("#form-close-btn")
+const searchFormContainer = selectElement("#search-form-container")
+fromOpenBtn.addEventListener("click",()=>{
+  searchFormContainer.classList.add("activated")
+})
+fromCloseBtn.addEventListener("click",()=>{
+  searchFormContainer.classList.remove("activated")
+})
+window.addEventListener("keyup", event=> {
+  if(event.key === "Escape") searchFormContainer.classList.remove("activated")
+} )
+// Swiper
+const swiper = new Swiper(".swiper", {
+    // How many slides to show
+    slidesPerView: 1,
+    // How much space between slides
+    spaceBetween: 20,
+    // Make the next and previous buttons work
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    // Make the pagination indicators work
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    //Responsive breakpoints for how many slides to show at that view
+    breakpoints: {
+        // 700px and up shoes 2 slides
+        700: {
+          slidesPerView: 2
+        },
+        // 1200px and up shoes 3 slides
+        1200: {
+            slidesPerView: 3
+        }
+    }   
+});
